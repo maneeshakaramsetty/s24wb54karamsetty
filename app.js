5e16c95a429f50a116bc422beebc737e742a067b
maneesha karamsetty <karamsettymaneesha@gmail.com>
2024-03-26 19:32:10 -0500

    Initial commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var herbsRouter = require('./routes/herbs');
+//var herbsRouter = require('./routes/herbs');
 
 var app = express();
 
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/herbs', herbsRouter);
+//app.use('/herbs', herbsRouter);
 
 
 // catch 404 and forward to error handler
